21d5f5b6ec544f36b50b2f93228c7729ff1ae421
Fernando Failla <f.foschiani@unifesp.br>
2025-12-10 15:51:56 -0300

    Add r-brackgroundjobs plugin

---
+++ lua/plugins/r-brackground.lua
@@ -0,0 +1,13 @@
+return{
+  {
+  'FernandoFailla/r-background-jobs.nvim',
+  branch = "dags",
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-telescope/telescope.nvim',  -- Optional
+  },
+  config = function()
+    require('r-background-jobs').setup()
+  end
+}
+}
