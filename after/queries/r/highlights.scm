e424e7b6a461ef3754c529d21dd2d9f99dbe441c
Fernando Failla <f.foschiani@unifesp.br>
2026-03-04 23:52:36 -0300

    feat: rich R syntax highlighting with kanso-zen color palette
    Treesitter queries (after/queries/r/highlights.scm):
    - Pipe operators (|>, %>%) as @keyword.operator
    - Assignment (<-, <<-, ->, ->>) and formula (~) operators
    - $ and @ extract operators with @variable.member for field names
    - Namespace pkg::func with @module highlight
    
    Color overrides (colorthemes.lua ColorScheme autocmd):
    - Pipes/assignment: warm red #c4746e (bold) to stand out
    - Parameters: sandy #b6927b (italic)
    - Fields/members: teal #8ea4a2
    - Functions: blue #8ba4b0 (bold for definitions)
    - Strings: warm green #8a9a7b
    - Constants (NULL/NA): red #c4746e (italic)
    - Booleans: amber #c4b28a (bold)
    - Numbers: purple #a292a3
    - Keywords: steel-blue #8992a7 (bold)
    - Snacks indent scope (#546e7a) and chunk (#75797f) guide colors
    - nvim-colorizer: enabled with virtualtext mode, inline preview

--- after/queries/r/highlights.scm
+++ after/queries/r/highlights.scm
@@ -1,6 +1,50 @@
 ;extends
+
+; Existing: quarto comment blocks
 (
 (comment) @comment
 (#match? @comment "^\\#\\|")
 ) @text.literal
 
+; Pipe operator |> gets a distinct highlight
+(binary_operator
+  operator: "|>" @keyword.operator)
+
+; Magrittr pipe %>% and other special operators (%in%, %*%, etc.)
+; "special" is an anonymous token, so we match it in the bracket syntax
+["special"] @keyword.operator
+
+; Assignment operators
+(binary_operator
+  operator: "<-" @keyword.operator)
+
+(binary_operator
+  operator: "<<-" @keyword.operator)
+
+(binary_operator
+  operator: "->" @keyword.operator)
+
+(binary_operator
+  operator: "->>" @keyword.operator)
+
+; Formula operator (~) used heavily in R
+(binary_operator
+  operator: "~" @keyword.operator)
+
+; $ and @ member access: highlight the rhs as a field
+(extract_operator
+  operator: "$" @punctuation.delimiter
+  rhs: (identifier) @variable.member)
+
+(extract_operator
+  operator: "@" @punctuation.delimiter
+  rhs: (identifier) @variable.member)
+
+; Namespace operator (pkg::func) - make pkg stand out
+(namespace_operator
+  lhs: (identifier) @module
+  operator: "::" @punctuation.delimiter)
+
+(namespace_operator
+  lhs: (identifier) @module
+  operator: ":::" @punctuation.delimiter)
--- lua/plugins/colorthemes.lua
+++ lua/plugins/colorthemes.lua
@@ -1,27 +1,64 @@
 return {
   {
-   "webhooked/kanso.nvim",
-   lazy = false,
-   enabled = true,
-   priority = 100,
-   config = function()
-      require("kanso").setup({
-        theme = "zen"
+    'webhooked/kanso.nvim',
+    lazy = false,
+    enabled = true,
+    priority = 100,
+    config = function()
+      require('kanso').setup {
+        theme = 'zen',
+      }
+      vim.cmd.colorscheme 'kanso-zen'
+      vim.api.nvim_create_autocmd('ColorScheme', {
+        pattern = '*',
+        callback = function()
+          vim.api.nvim_set_hl(0, 'SnacksTerminal', { bg = 'none', nocombine = true })
+          vim.api.nvim_set_hl(0, 'SnacksPickerBorder', { fg = '#316c71', bg = 'none', nocombine = true })
+          -- better indent guides
+          vim.api.nvim_set_hl(0, 'SnacksIndent', { fg = '#2a2a2a', nocombine = true })
+          vim.api.nvim_set_hl(0, 'SnacksIndentScope', { fg = '#546e7a', nocombine = true })
+          vim.api.nvim_set_hl(0, 'SnacksIndentChunk', { fg = '#75797f', nocombine = true })
+
+          -- richer R syntax colors (works with kanso-zen's palette)
+          -- pipe/assignment/formula operators: warm accent to stand out from arithmetic
+          vim.api.nvim_set_hl(0, '@keyword.operator.r', { fg = '#c4746e', bold = true })
+          -- named parameters in function calls: subtle but distinct
+          vim.api.nvim_set_hl(0, '@variable.parameter.r', { fg = '#b6927b', italic = true })
+          -- $ and @ field access (column names): distinct from plain variables
+          vim.api.nvim_set_hl(0, '@variable.member.r', { fg = '#8ea4a2' })
+          -- namespace (pkg:: prefix): muted teal
+          vim.api.nvim_set_hl(0, '@module.r', { fg = '#8ea4a2', italic = true })
+          -- function calls: keep the blue but make bolder
+          vim.api.nvim_set_hl(0, '@function.call.r', { fg = '#8ba4b0' })
+          -- function definitions: blue + bold
+          vim.api.nvim_set_hl(0, '@function.r', { fg = '#8ba4b0', bold = true })
+          -- strings: warmer green
+          vim.api.nvim_set_hl(0, '@string.r', { fg = '#8a9a7b' })
+          -- constants (NULL, NA, Inf, NaN): orange-ish to warn
+          vim.api.nvim_set_hl(0, '@constant.builtin.r', { fg = '#c4746e', italic = true })
+          -- booleans: warm amber
+          vim.api.nvim_set_hl(0, '@boolean.r', { fg = '#c4b28a', bold = true })
+          -- numbers: purple-ish to contrast with strings
+          vim.api.nvim_set_hl(0, '@number.r', { fg = '#a292a3' })
+          vim.api.nvim_set_hl(0, '@number.float.r', { fg = '#a292a3' })
+          -- keywords (if, else, for, while, function, return): bold purple
+          vim.api.nvim_set_hl(0, '@keyword.r', { fg = '#8992a7', bold = true })
+          vim.api.nvim_set_hl(0, '@keyword.function.r', { fg = '#8992a7', bold = true, italic = true })
+          vim.api.nvim_set_hl(0, '@keyword.return.r', { fg = '#8992a7', bold = true, italic = true })
+          vim.api.nvim_set_hl(0, '@keyword.conditional.r', { fg = '#8992a7', bold = true })
+          vim.api.nvim_set_hl(0, '@keyword.repeat.r', { fg = '#8992a7', bold = true })
+          -- comments: keep italic, slightly brighter
+          vim.api.nvim_set_hl(0, '@comment.r', { fg = '#7b8084', italic = true })
+          -- operators (arithmetic, comparison): neutral but visible
+          vim.api.nvim_set_hl(0, '@operator.r', { fg = '#a0a4a8' })
+        end,
       })
-     vim.cmd.colorscheme 'kanso-zen'
-     vim.api.nvim_create_autocmd("ColorScheme", {
-       pattern = "*",
-       callback = function()
-         vim.api.nvim_set_hl(0, "SnacksTerminal", { bg = "none", nocombine = true })
-         vim.api.nvim_set_hl(0, "SnacksPickerBorder", { fg = "#316c71", bg = "none", nocombine = true })
-       end,
-     })
     end,
   },
   { 'webhooked/oscura.nvim', enabled = true, lazy = false, priority = 1000, opts = {} },
   { 'Mofiqul/vscode.nvim', enabled = true, lazy = false, priority = 1000, opts = {} },
   { 'slugbyte/lackluster.nvim', enabled = true, lazy = false, priority = 1000, opts = {} },
-  { 'projekt0n/github-nvim-theme', enabled = true, lazy = false, priority = 1000 } ,
+  { 'projekt0n/github-nvim-theme', enabled = true, lazy = false, priority = 1000 },
   { 'forest-nvim/sequoia.nvim', enabled = false, lazy = false, priority = 1000 },
   { 'shaunsingh/nord.nvim', enabled = false, lazy = false, priority = 1000 },
   { 'folke/tokyonight.nvim', enabled = false, lazy = false, priority = 1000 },
@@ -80,27 +117,29 @@ return {
   -- color html colors
   {
     'NvChad/nvim-colorizer.lua',
-    enabled = false,
+    enabled = true,
+    event = 'BufReadPost',
     opts = {
       filetypes = { '*' },
       user_default_options = {
         RGB = true, -- #RGB hex codes
         RRGGBB = true, -- #RRGGBB hex codes
-        names = true, -- "Name" codes like Blue or blue
+        names = false, -- "Name" codes like Blue or blue (too many false positives)
         RRGGBBAA = true, -- #RRGGBBAA hex codes
         AARRGGBB = false, -- 0xAARRGGBB hex codes
-        rgb_fn = false, -- CSS rgb() and rgba() functions
-        hsl_fn = false, -- CSS hsl() and hsla() functions
+        rgb_fn = true, -- CSS rgb() and rgba() functions
+        hsl_fn = true, -- CSS hsl() and hsla() functions
         css = false, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
         css_fn = false, -- Enable all CSS *functions*: rgb_fn, hsl_fn
         -- Available modes for `mode`: foreground, background,  virtualtext
-        mode = 'background', -- Set the display mode.
+        mode = 'virtualtext', -- Set the display mode.
         -- Available methods are false / true / "normal" / "lsp" / "both"
         -- True is same as normal
         tailwind = false, -- Enable tailwind colors
         -- parsers can contain values used in |user_default_options|
         sass = { enable = false, parsers = { 'css' } }, -- Enable sass colors
-        virtualtext = '■',
+        virtualtext = '  ',
+        virtualtext_inline = true,
         -- update color values even if buffer is not focused
         -- example use: cmp_menu, cmp_docs
         always_update = false,
